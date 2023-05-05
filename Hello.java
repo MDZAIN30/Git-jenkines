4cea87490831f395e3e063c1e3fddd0793dce6f1
MDZAIN30 <128170026+MDZAIN30@users.noreply.github.com>
2023-05-05 13:26:02 +0530

    First update

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello
 {
    public static void main(String[]args)
 {
-  System.out.println("Hello zain workhard you can do this");
+  System.out.println("Hello zain  workhard and studywell you can do this");
 
 }
 
